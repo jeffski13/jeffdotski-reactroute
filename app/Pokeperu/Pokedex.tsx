80de76ae26057ebe6fc5abe286a9925f1d1310ca
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-05-09 09:29:43 -0500

    orgnazing and typing the pokemon array

--- app/Pokeperu/Pokedex.tsx
+++ app/Pokeperu/Pokedex.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import ROUTES from '../consts/ROUTES';
+import { monsters, type Monster } from './monsters';
 
-interface Monster {
-  name: string;
-  type: string;
-  secondaryType?: string;
-}
-
-const monsters: Monster[] = [
-  { name: 'Bulbasaur', type: 'Grass', secondaryType: 'Poison' },
-  { name: 'Charmander', type: 'Fire' },
-  { name: 'Squirtle', type: 'Water' },
-  { name: 'Pikachu', type: 'Electric' },
-  { name: 'Jigglypuff', type: 'Normal', secondaryType: 'Fairy' },
-  { name: 'Eevee', type: 'Normal' },
-];
 
 export default function Pokedex() {
 
@@ -23,11 +10,11 @@ export default function Pokedex() {
       <a href={ROUTES.pokePeru.battle}>Back</a>
       <h1>Pokedex</h1>
       <ul className="monster-list">
-        {monsters.map((monster) => (
+        {monsters.map((monster: Monster) => (
           <li key={monster.name} className="monster-item">
             <h2>{monster.name}</h2>
             <p>Type: {monster.type}</p>
-            {monster.secondaryType && <p>Secondary Type: {monster.secondaryType}</p>}
+            {monster.secondType && <p>Secondary Type: {monster.secondType}</p>}
           </li>
         ))}
       </ul>
---
+++ app/Pokeperu/monsters.ts
@@ -0,0 +1,167 @@
+import { MonsterType } from './MonsterType';
+
+// Define the structure of an attack
+interface MonsterAttack {
+  name: string;
+  damage: number;
+  type: MonsterType;
+}
+
+// Define the structure of a monster
+export interface Monster {
+  name: string;
+  trainer: string;
+  hp: number;
+  attack: number;
+  defense: number;
+  specialAttack: number;
+  specialDefense: number;
+  speed: number;
+  type: MonsterType;
+  secondType: MonsterType | null;
+  image: string;
+  attack1: MonsterAttack;
+  attack2: MonsterAttack;
+}
+
+// Use the Monster interface for the monsters array
+export const monsters: Monster[] = [
+  {
+    name: 'Pikachu',
+    trainer: 'Lt. Surge',
+    hp: 35,
+    attack: 55,
+    defense: 40,
+    specialAttack: 50,
+    specialDefense: 50,
+    speed: 90,
+    type: MonsterType.Electric,
+    secondType: null,
+    image: '/images/pikachu.jpg',
+    attack1: {
+      name: 'Quick Attack',
+      damage: 10,
+      type: MonsterType.Normal,
+    },
+    attack2: {
+      name: 'Thunderbolt',
+      damage: 20,
+      type: MonsterType.Electric,
+    },
+  },
+  {
+    name: 'Charmander',
+    trainer: 'Blaine',
+    hp: 39,
+    attack: 52,
+    defense: 43,
+    specialAttack: 60,
+    specialDefense: 50,
+    speed: 65,
+    type: MonsterType.Fire,
+    secondType: null,
+    image: '/images/charmander.jpg',
+    attack1: {
+      name: 'Scratch',
+      damage: 10,
+      type: MonsterType.Normal,
+    },
+    attack2: {
+      name: 'Flamethrower',
+      damage: 20,
+      type: MonsterType.Fire,
+    },
+  },
+  {
+    name: 'Bulbasaur',
+    trainer: 'Erika',
+    hp: 45,
+    attack: 49,
+    defense: 49,
+    specialAttack: 65,
+    specialDefense: 65,
+    speed: 45,
+    type: MonsterType.Grass,
+    secondType: MonsterType.Poison,
+    image: '/images/bulbasaur.jpg',
+    attack1: {
+      name: 'Tackle',
+      damage: 10,
+      type: MonsterType.Normal,
+    },
+    attack2: {
+      name: 'Vine Whip',
+      damage: 20,
+      type: MonsterType.Grass,
+    },
+  },
+  {
+    name: 'Squirtle',
+    trainer: 'Misty',
+    hp: 44,
+    attack: 48,
+    defense: 65,
+    specialAttack: 50,
+    specialDefense: 64,
+    speed: 43,
+    type: MonsterType.Water,
+    secondType: null,
+    image: '/images/squirtle.png',
+    attack1: {
+      name: 'Tackle',
+      damage: 10,
+      type: MonsterType.Normal,
+    },
+    attack2: {
+      name: 'Water Gun',
+      damage: 20,
+      type: MonsterType.Water,
+    },
+  },
+  {
+    name: 'Eevee',
+    trainer: 'Erika',
+    hp: 55,
+    attack: 55,
+    defense: 50,
+    specialAttack: 45,
+    specialDefense: 65,
+    speed: 55,
+    type: MonsterType.Normal,
+    secondType: null,
+    image: '/images/eevee.png',
+    attack1: {
+      name: 'Quick Attack',
+      damage: 10,
+      type: MonsterType.Normal,
+    },
+    attack2: {
+      name: 'Bite',
+      damage: 20,
+      type: MonsterType.Dark,
+    },
+  },
+  {
+    name: 'Jigglypuff',
+    trainer: 'Whitney',
+    hp: 115,
+    attack: 45,
+    defense: 20,
+    specialAttack: 45,
+    specialDefense: 25,
+    speed: 20,
+    type: MonsterType.Normal,
+    secondType: MonsterType.Fairy,
+    image: '/images/jigglypuff.jpg',
+    attack1: {
+      name: 'Pound',
+      damage: 10,
+      type: MonsterType.Normal,
+    },
+    attack2: {
+      name: 'Sing',
+      damage: 0,
+      type: MonsterType.Fairy,
+    },
+  },
+];
