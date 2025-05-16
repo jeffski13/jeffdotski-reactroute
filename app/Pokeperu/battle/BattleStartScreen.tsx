6b008924471e7c64fe02530bc3d07c001cdf5343
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-05-16 17:10:24 -0500

    styling for results of monster chosen appearing above the monster/trainer

--- app/Pokeperu/battle/BattleStartScreen.tsx
+++ app/Pokeperu/battle/BattleStartScreen.tsx
@@ -38,38 +38,42 @@ export default function BattleStartScreen({
       <div className="selection-results-container">
         {/* User 1 */}
         <div className="monster-trainer-container" style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{ position: 'relative', width: 220, height: 200 }}>
-            <img
-              src={trainer1Image}
-              alt="Trainer 1"
-              className="trainer-image trainer1"
-            />
-            <img
-              src={monster1Image}
-              alt={monster1}
-              className="monster-selected monster1"
-            />
-          </div>
-          <div style={{ marginLeft: 16 }}>
-            <p>User 1 chose: {monster1}</p>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div style={{ marginBottom: 8, fontWeight: 'bold', textAlign: 'center' }}>
+              User 1 chose: {monster1}
+            </div>
+            <div style={{ position: 'relative', width: 220, height: 200 }}>
+              <img
+                src={trainer1Image}
+                alt="Trainer 1"
+                className="trainer-image trainer1"
+              />
+              <img
+                src={monster1Image}
+                alt={monster1}
+                className="monster-selected monster1"
+              />
+            </div>
           </div>
         </div>
         {/* User 2 */}
         <div className="monster-trainer-container" style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{ marginRight: 16 }}>
-            <p>User 2 chose: {monster2}</p>
-          </div>
-          <div style={{ position: 'relative', width: 220, height: 200 }}>
-            <img
-              src={trainer2Image}
-              alt="Trainer 2"
-              className="trainer-image trainer2"
-            />
-            <img
-              src={monster2Image}
-              alt={monster2}
-              className="monster-selected monster2"
-            />
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div style={{ marginBottom: 8, fontWeight: 'bold', textAlign: 'center' }}>
+              User 2 chose: {monster2}
+            </div>
+            <div style={{ position: 'relative', width: 220, height: 200 }}>
+              <img
+                src={trainer2Image}
+                alt="Trainer 2"
+                className="trainer-image trainer2"
+              />
+              <img
+                src={monster2Image}
+                alt={monster2}
+                className="monster-selected monster2"
+              />
+            </div>
           </div>
         </div>
       </div>
