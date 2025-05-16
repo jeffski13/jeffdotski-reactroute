38c7472202902ff7288ab8e185862dd4916f52ee
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-05-16 17:58:28 -0500

    mobile friendly for monster results

--- app/Pokeperu/battle/BattleStartScreen.tsx
+++ app/Pokeperu/battle/BattleStartScreen.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import './battlestart.css';
 
 interface MonsterSelectionResultsProps {
@@ -33,55 +33,64 @@ export default function BattleStartScreen({
   }, [setBattleClicked]);
 
   return (
-    <div className="MonsterSelectionResults">
-      <h1>Selection Results</h1>
-      <div className="selection-results-container">
-        {/* User 1 */}
-        <div className="monster-trainer-container" style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <div style={{ marginBottom: 8, fontWeight: 'bold', textAlign: 'center' }}>
-              User 1 chose: {monster1}
+    <Container>
+      <Row>
+        <h1>Selection Results</h1>
+      </Row>
+      <Row>
+        <Col style={{ position: 'relative' }}>
+          {/* User 1 */}
+          <div className="monster-trainer-container" style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <div style={{ marginBottom: 8, fontWeight: 'bold', textAlign: 'center' }}>
+                User 1 chose: {monster1}
+              </div>
+              <div style={{ position: 'relative', width: 220, height: 200 }}>
+                <img
+                  src={trainer1Image}
+                  alt="Trainer 1"
+                  className="trainer-image trainer1"
+                />
+                <img
+                  src={monster1Image}
+                  alt={monster1}
+                  className="monster-selected monster1"
+                />
+              </div>
             </div>
-            <div style={{ position: 'relative', width: 220, height: 200 }}>
-              <img
-                src={trainer1Image}
-                alt="Trainer 1"
-                className="trainer-image trainer1"
-              />
-              <img
-                src={monster1Image}
-                alt={monster1}
-                className="monster-selected monster1"
-              />
-            </div>
-          </div>
-        </div>
-        {/* VS */}
-        <div className="monster-trainer-container" style={{ display: 'flex', alignItems: 'center' }}>
-          <h2>VS</h2>
-        </div>
-        {/* User 2 */}
-        <div className="monster-trainer-container" style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <div style={{ marginBottom: 8, fontWeight: 'bold', textAlign: 'center' }}>
-              User 2 chose: {monster2}
-            </div>
-            <div style={{ position: 'relative', width: 220, height: 200 }}>
-              <img
-                src={trainer2Image}
-                alt="Trainer 2"
-                className="trainer-image trainer2"
-              />
-              <img
-                src={monster2Image}
-                alt={monster2}
-                className="monster-selected monster2"
-              />
+            <div className='results-vs' style={{ marginLeft: 'auto', alignSelf: 'flex-start' }}>
+              <h2>VS</h2>
             </div>
           </div>
-        </div>
-      </div>
-      <Button onClick={setBattleClicked}>Start Battle</Button>
-    </div>
+        </Col>
+        <Col>
+          {/* User 2 */}
+          <div className="monster-trainer-container" style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <div style={{ marginBottom: 8, fontWeight: 'bold', textAlign: 'center' }}>
+                User 2 chose: {monster2}
+              </div>
+              <div style={{ position: 'relative', width: 220, height: 200 }}>
+                <img
+                  src={trainer2Image}
+                  alt="Trainer 2"
+                  className="trainer-image trainer2"
+                />
+                <img
+                  src={monster2Image}
+                  alt={monster2}
+                  className="monster-selected monster2"
+                />
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="startButtonContainer" xs={12}>
+          <Button onClick={setBattleClicked}>Start Battle</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
