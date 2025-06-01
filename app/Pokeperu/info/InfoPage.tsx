740c117f3b454e168371d550d6e895029d8f5576
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-06-01 16:56:33 -0500

    info modifications

--- app/Pokeperu/info/InfoPage.tsx
+++ app/Pokeperu/info/InfoPage.tsx
@@ -31,7 +31,7 @@ function InfoPage({ }: InfoPageProps) {
           <li>Personality Types</li>
         </ul>
         <p>The students were required to create pokemon (inspired by animals) and a gym leader (with a personality). The project had an optional battle component with prizes to keep the students invested in the project.</p>
-        <div id="sectionPokemonCreation">
+        <div className="infoSection" id="sectionPokemonCreation">
           <h3>Pokemon Creation</h3>
           <p>The students were required to create the following:</p>
           <ul>
@@ -46,13 +46,13 @@ function InfoPage({ }: InfoPageProps) {
           <p>The "description" was an opportunity for the students to utilize more scientific, formal english.</p>
           <Container>
             <Row className="info-images-container">
-              <Col sm={5}>
+              <Col sm={11} md={5}>
                 <img src="/images/info/info_pokemon_creation_a.png" alt="Back" className="info-image" />
               </Col>
-              <Col sm={1} className="info-arrow-separator" >
+              <Col sm={1}  className="info-arrow-separator" >
                 <span>➡️</span>
               </Col>
-              <Col sm={5}>
+              <Col sm={12} md={5}>
                 <img src="/images/info/info_pokemon_creation_b.png" alt="Back" className="info-image" />
               </Col>
             </Row>
@@ -60,7 +60,7 @@ function InfoPage({ }: InfoPageProps) {
           <p>This data can be found and modified inside of the "monsters.tsx" file.</p>
           <p>The art from the students can be placed inside of the /public/images/monsters folder.</p>
         </div>
-        <div id="sectionGymLeaderCreation">
+        <div className="infoSection" id="sectionGymLeaderCreation">
           <h3>Gym Leader Creation</h3>
           <p>The students were required to create the following:</p>
           <ul>
@@ -71,13 +71,13 @@ function InfoPage({ }: InfoPageProps) {
           <p>The "personality" was an opportunity for the students to utilize vocabulary related to mannerisms and behavior.</p>
           <Container>
             <Row className="info-images-container">
-              <Col sm={5}>
+              <Col sm={11} md={5}>
                 <img src="/images/info/info_gymleader_creation_a.png" alt="Back" className="info-image" />
               </Col>
-              <Col sm={1} className="info-arrow-separator" >
+              <Col sm={1}  className="info-arrow-separator" >
                 <span>➡️</span>
               </Col>
-              <Col sm={5}>
+              <Col sm={12} md={5}>
                 <img src="/images/info/info_gymleader_creation_b.png" alt="Back" className="info-image" />
               </Col>
             </Row>
@@ -86,23 +86,24 @@ function InfoPage({ }: InfoPageProps) {
           <p>The art from the students can be placed inside of the /public/images/gymleaders folder.</p>
           <p>The environments can be placed inside of the /public/images/perulandscape folder.</p>
         </div>
-        <div id="sectionBattle">
+        <div className="infoSection" id="sectionBattle">
           <h3>Monster Battle</h3>
           <p>As a celebration of the end of the project, the students were able to battle one another. Students were also given an opportunity to experiment beforehand and run battle simulations to see if their monster would perform well in the heat of battle.</p>
           <Container>
             <Row className="info-images-container">
-              <Col sm={5}>
+              <Col sm={11} md={5}>
                 <img src="/images/info/info_battle_a.png" alt="Back" className="info-image" />
               </Col>
-              <Col sm={1} className="info-arrow-separator" >
+              <Col sm={1}  className="info-arrow-separator" >
+                <span>💥</span>
               </Col>
-              <Col sm={5}>
+              <Col sm={12} md={5}>
                 <img src="/images/info/info_battle_b.png" alt="Back" className="info-image" />
               </Col>
             </Row>
           </Container>
         </div>
-        <div id="sectionDIY">
+        <div className="infoSection" id="sectionDIY">
           <h3>Can I Do This With My Students?</h3>
           <p>Absolutely! This game is built with node v20.9.0. The contents can be downloaded from my github repo and then run the following commands inside of the unzipped folder:</p>
           <p>npm install</p>
@@ -111,13 +112,13 @@ function InfoPage({ }: InfoPageProps) {
           <p>The <strong>monsters.tsx</strong> and <strong>gymleaders.tsx</strong> files can be modified with the respective monster and gym leader data.</p>
           <Container>
             <Row className="info-images-container">
-              <Col sm={5}>
+              <Col sm={11} md={5}>
                 <img src="/images/info/info_pokemon_creation_dataentry_a.png" alt="Back" className="info-image" />
               </Col>
-              <Col sm={1} className="info-arrow-separator" >
+              <Col sm={1}  className="info-arrow-separator" >
                 <span>➡️</span>
               </Col>
-              <Col sm={5}>
+              <Col sm={12} md={5}>
                 <img src="/images/info/info_pokemon_creation_dataentry_b.png" alt="Back" className="info-image" />
               </Col>
             </Row>
@@ -125,13 +126,13 @@ function InfoPage({ }: InfoPageProps) {
           <br />
           <Container>
             <Row className="info-images-container">
-              <Col sm={5}>
+              <Col sm={11} md={5}>
                 <img src="/images/info/info_gymleader_creation_dataentry_a.png" alt="Back" className="info-image" />
               </Col>
-              <Col sm={1} className="info-arrow-separator" >
+              <Col sm={1}  className="info-arrow-separator" >
                 <span>➡️</span>
               </Col>
-              <Col sm={5}>
+              <Col sm={12} md={5}>
                 <img src="/images/info/info_gymleader_creation_dataentry_b.png" alt="Back" className="info-image" />
               </Col>
             </Row>
